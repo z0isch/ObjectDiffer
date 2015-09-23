55b1aaf69cf2aaab7a0e44989d3e8689dc344575
Dave Martin <dmartin@sportingindex.com>
2015-09-23 14:01:13 +0100

    added differ factory and tests. Resolves #2

---
+++ ObjectDiffer.Test/DifferFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ObjectDiffer.TypeDiffers;
+
+namespace ObjectDiffer.Test
+{
+    [TestFixture]
+    public class DifferFactoryTests
+    {
+        private DifferFactory _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new DifferFactory();
+        }
+
+        [Test]
+        public void DefaultDifferShouldNotBeNull()
+        {
+            Assert.IsNotNull(_sut.GetDefault());
+        }
+
+        [Test]
+        public void DifferWithTypeDiffersShouldNotBeNull()
+        {
+            Assert.IsNotNull(_sut.GetWithTypeDiffers(new List<ITypeDiffer>()));
+        }
+    }
+}
---
+++ ObjectDiffer/DifferFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ObjectDiffer.TypeDiffers;
+
+namespace ObjectDiffer
+{
+    public class DifferFactory
+    {
+        public IDiffer GetDefault()
+        {
+            return new Differ(new List<ITypeDiffer>
+            {
+                new PrimativeDiffer(),
+                new EnumerableDiffer(new HashCodeEqualityComparer()),
+                new ObjectTypeDiffer()
+            });
+        }
+
+        public IDiffer GetWithTypeDiffers(IEnumerable<ITypeDiffer> typeDiffers)
+        {
+            return new Differ(typeDiffers);
+        }
+    }
+}
