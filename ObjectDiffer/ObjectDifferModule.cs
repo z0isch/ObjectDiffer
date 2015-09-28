2a68fe21de97fd7d661f03c4f4b5f3289c9e9dbe
Dave Martin <dmartin@sportingindex.com>
2015-09-28 10:35:27 +0100

    added support for diffing arrays by index to ninject module

--- ObjectDiffer/ObjectDifferModule.cs
+++ ObjectDiffer/ObjectDifferModule.cs
@@ -7,10 +7,25 @@ namespace ObjectDiffer
 {
     public class ObjectDifferModule : NinjectModule
     {
+        private readonly bool _diffEnumerablesByIndex;
+
+        public ObjectDifferModule(bool diffEnumerablesByIndex = false)
+        {
+            _diffEnumerablesByIndex = diffEnumerablesByIndex;
+        }
+
         public override void Load()
         {
             Bind<ITypeDiffer>().To<PrimativeDiffer>();
-            Bind<ITypeDiffer>().To<ObjectEqualityEnumerableDiffer>();
+
+            if (_diffEnumerablesByIndex)
+            {
+                Bind<ITypeDiffer>().To<IndexEnumerableDiffer>();
+            }
+            else
+            {
+                Bind<ITypeDiffer>().To<ObjectEqualityEnumerableDiffer>();
+            }
 
             Bind<ITypeDiffer>().To<ObjectTypeDiffer>();
             Bind<IEqualityComparer<object>>().To<DefaultEqualityComparer>().Named("SameObjectComparer");
